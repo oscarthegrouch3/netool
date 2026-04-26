016f360d08d102f2c6709d17c69101b5d1154d90
oscar <oscarlh203@gmail.com>
2026-04-27 01:12:46 +1000

    add hostname indentification

--- subnet.py
+++ subnet.py
@@ -1,9 +1,6 @@
-# Look up on local network
-# Get device information e.g MAC, Device Manufacture and online device
-
-
 from scapy.all import ARP, Ether, srp
 from scapy.data import MANUFDB
+import socket
 
 def subnet_scan(subnet):
     answered, _ = srp(Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=subnet), timeout=5, retry=3, verbose=False)
@@ -11,12 +8,18 @@ def subnet_scan(subnet):
 
     for _, rcv in answered:
         vendor = MANUFDB.lookup(rcv.hwsrc)
-        hosts[rcv.psrc] = {"mac": rcv.hwsrc, "vendor": vendor}
+        try:
+            hostname = socket.gethostbyaddr(rcv.psrc)[0]
+        except socket.herror:
+            hostname = None
 
-    
+        hosts[rcv.psrc] = {
+            "mac":      rcv.hwsrc,
+            "vendor":   vendor,
+            "hostname": hostname,
+        }
+
     return hosts
-
-
 if __name__ == "__main__":
     result = subnet_scan("192.168.200.26/24")
     print(result)
